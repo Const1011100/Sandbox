233b8b244ecddb1115b57d4c8775638052277c95
Const1011100 <const1011100@gmail.com>
2023-12-23 20:03:35 +0200

    Codewars Task №85, 86

--- js/codewars_81_100.js
+++ js/codewars_81_100.js
@@ -73,4 +73,140 @@ class Kata {
 
 // ================================================================================================
 // ================================================================================================
-//*-залишилось 298шт
+//*-Task №85 Define a card suit (8kyu)
+/*
+Ви отримуєте будь-яку картку як аргумент. Ваше завдання повернути масть цієї карти (у нижньому регістрі).
+Наша колода (попередньо завантажена):
+('3♣') -> return 'clubs'
+('3♦') -> return 'diamonds'
+('3♥') -> return 'hearts'
+('3♠') -> return 'spades'
+*/
+
+// Мій варіант
+function defineSuit(card) {
+    const suit = card[card.length - 1];
+    const suits = {
+        '♣': 'clubs',
+        '♦': 'diamonds',
+        '♥': 'hearts',
+        '♠': 'spades',
+    }
+    return suits[suit];
+}
+
+// Цікаві варіанти з Codewars
+function defineSuit_(card) {
+    return {
+        '♣': 'clubs',
+        '♦': 'diamonds',
+        '♥': 'hearts',
+        '♠': 'spades'
+    }[card.substr(-1)]
+}
+//
+function defineSuit__(card) {
+    switch (card.slice(-1)) {
+        case '♣': return 'clubs';
+        case '♦': return 'diamonds';
+        case '♥': return 'hearts';
+        case '♠': return 'spades';
+    }
+}
+// ================================================================================================
+// ================================================================================================
+//*-Task №86 Crash Override (8kyu)
+/*
+Кожному починаючому хакеру потрібен псевдонім! Phantom Phreak, Acid Burn, Zero Cool і Crash Override
+– деякі яскраві приклади з фільму Хакери.
+Ваше завдання — створити функцію, яка за належного імені та прізвища повертатиме правильний псевдонім.
+
+Примітки:
+Два об’єкти, які повертають ім’я з одного слова у відповідь на першу літеру імені та один на першу
+літеру прізвища, вже задані. Перегляньте наведені нижче приклади для отримання додаткової інформації.
+
+Якщо перший символ будь-якого з імен, наданих функції, не є літерою від A до Z, ви повинні повернути
+«Ваше ім’я має починатися з літери від A до Z» / "Your name must start with a letter from A - Z.".
+
+Іноді люди можуть забути написати першу літеру свого імені великою, тому ваша функція повинна
+врахувати ці граматичні помилки.
+*/
+
+// Мій варіант
+function aliasGen(firstName, lastName){
+    const first = firstName[0].toUpperCase();
+    const last = lastName[0].toUpperCase();
+    const firstNameAlias = {
+            A: 'Alpha',
+            B: 'Beta',
+            C: 'Cache',
+            D: 'Data',
+            E: 'Energy',
+            F: 'Function',
+            G: 'Glitch',
+            H: 'Half-life',
+            I: 'Ice',
+            J: 'Java',
+            K: 'Keystroke',
+            L: 'Logic',
+            M: 'Malware',
+            N: 'Nagware',
+            O: 'OS',
+            P: 'Phishing',
+            Q: 'Quantum',
+            R: 'RAD',
+            S: 'Strike',
+            T: 'Trojan',
+            U: 'Ultraviolet',
+            V: 'Vanilla',
+            W: 'WiFi',
+            X: 'Xerox',
+            Y: 'Y',
+            Z: 'Zero'
+        };
+    const lastNameAlias = {
+            A: 'Analogue',
+            B: 'Bomb',
+            C: 'Catalyst',
+            D: 'Discharge',
+            E: 'Electron',
+            F: 'Faraday',
+            G: 'Gig',
+            H: 'Hacker',
+            I: 'IP',
+            J: 'Jabber',
+            K: 'Killer',
+            L: 'Lazer',
+            M: 'Mike',
+            N: 'n00b',
+            O: 'Overclock',
+            P: 'Payload',
+            Q: 'Quark',
+            R: 'Roy',
+            S: 'Spy',
+            T: 'T-Rex',
+            U: 'Unit',
+            V: 'Virus',
+            W: 'Worm',
+            X: 'X',
+            Y: 'Yob',
+            Z: 'Zombie'
+        };
+    if(firstNameAlias.hasOwnProperty(first) && lastNameAlias.hasOwnProperty(last)) {
+      return `${firstNameAlias[first]} ${lastNameAlias[last]}`;
+    } else {
+      return "Your name must start with a letter from A - Z.";
+    }
+}
+
+// Цікаві варіанти з Codewars
+const initialCap = (str) => str[0].toUpperCase();
+const isValidName = (name) => /^[a-z]/i.test(name);
+const aliasGen = (fName, lName) => {
+  return (isValidName(fName) && isValidName(lName))
+    ? `${ firstName[initialCap(fName)] } ${ surname[initialCap(lName)] }`
+    : 'Your name must start with a letter from A - Z.';
+}
+// ================================================================================================
+// ================================================================================================
+//*-залишилось 296шт
