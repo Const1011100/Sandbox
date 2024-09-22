52663797453a9f4bafbf30ae0d20da15ad0d9360
Const1011100 <const1011100@gmail.com>
2024-09-22 14:33:38 +0300

    Codewars Task №502

--- js/codewars_501-520.js
+++ js/codewars_501-520.js
@@ -18,6 +18,116 @@ function bump__(x) {
 }
 // ================================================================================================
 // ================================================================================================
-//*-залишилось 1871шт
+//*-Task №502 Alphabet war (7kyu)
+/*
+Йде війна, і ніхто не знає - алфавітна війна!
+Розрізняють дві групи ворожих листів. Напруга між літерами з лівої та правої сторони була надто
+високою, і почалася війна.
+
+Завдання
+Напишіть функцію, яка приймає рядок бою, що складається лише з маленьких літер, і повертає, хто
+переміг у бою. Коли виграє ліва сторона, поверніться, виграє ліва сторона!, коли виграє права, поверніться,
+виграє права сторона!, в іншому випадку поверніться, Давайте битись знову!.
+
+Ліві літери та їх потужність:
+w - 4
+p - 3
+b - 2
+s - 1
+
+Праві літери та їх потужність:
+
+m - 4
+q - 3
+d - 2
+z - 1
+Інші букви не мають сили і є лише жертвами.
+
+приклад
+alphabetWar("z"); //=> Перемагає права сторона!
+alphabetWar("zdqmwpbs"); //=> Давайте битися знову!
+alphabetWar("zzzzs"); //=> Перемагає права сторона!
+alphabetWar("wwwwwwz"); //=> Перемагає ліва сторона!
+*/
+
+// Мій варіант
+function alphabetWar(fight) {
+    const leftSide = {
+        w: 4,
+        p: 3,
+        b: 2,
+        s: 1,
+    };
+    const rightSide = {
+        m: 4,
+        q: 3,
+        d: 2,
+        z: 1,
+    };
+    let leftSum = 0;
+    let rightSum = 0;
+    fight.split('').forEach((elem) => {
+        if (leftSide[elem]) {
+            leftSum += leftSide[elem];
+        }
+        if (rightSide[elem]) {
+            rightSum += rightSide[elem];
+        }
+    });
+    return leftSum > rightSum
+        ? 'Left side wins!'
+        : leftSum < rightSum
+        ? 'Right side wins!'
+        : "Let's fight again!";
+}
+// Цікаві варіанти з Codewars
+function alphabetWar_(fight) {
+    let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+    let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+    return result
+        ? (result < 0 ? 'Left' : 'Right') + ' side wins!'
+        : "Let's fight again!";
+}
+//
+function alphabetWar__(fight) {
+    let score = 0;
+    for (const letter of fight) {
+        switch (letter) {
+            case 'w':
+                score -= 4;
+                break;
+            case 'p':
+                score -= 3;
+                break;
+            case 'b':
+                score -= 2;
+                break;
+            case 's':
+                score -= 1;
+                break;
+            case 'm':
+                score += 4;
+                break;
+            case 'q':
+                score += 3;
+                break;
+            case 'd':
+                score += 2;
+                break;
+            case 'z':
+                score += 1;
+                break;
+        }
+    }
+
+    return score < 0
+        ? 'Left side wins!'
+        : score > 0
+        ? 'Right side wins!'
+        : "Let's fight again!";
+}
+// ================================================================================================
+// ================================================================================================
+//*-залишилось 1870шт
 // console.time('timer_1');
 // console.timeEnd('timer_1');
