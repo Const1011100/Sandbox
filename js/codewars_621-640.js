7957356b4086bf3904bf183c70cdd1899aac3c84
Const1011100 <const1011100@gmail.com>
2025-02-02 20:41:11 +0200

    Codewars Task №630

--- js/codewars_621-640.js
+++ js/codewars_621-640.js
@@ -245,6 +245,32 @@ function differenceOfSquares_(x) {
 }
 // ================================================================================================
 // ================================================================================================
-//*-залишилось 1750шт
+//*-Task №630 Remove All The Marked Elements of a List (7kyu)
+/*
+Визначте метод/функцію, яка видаляє з заданого масиву цілих чисел усі значення, що містяться у другому масиві.
+Приклади (введення -> вихід):
+* [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+* [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+* [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+*/
+
+// Мій варіант
+Array.prototype.remove_ = function (integer_list, values_list) {
+  return integer_list.filter((num) => !values_list.includes(num));
+};
+// Цікаві варіанти з Codewars
+Array.prototype.remove__ = function (integer_list, values_list) {
+  return integer_list.filter(function (element) {
+    return values_list.indexOf(element) === -1;
+  });
+};
+//
+Array.prototype.remove___ = function (integer_list, values_list) {
+  const s = new Set(values_list);
+  return integer_list.filter((x) => !s.has(x));
+};
+// ================================================================================================
+// ================================================================================================
+//*-залишилось 1749шт
 // console.time('timer_1');
 // console.timeEnd('timer_1');
