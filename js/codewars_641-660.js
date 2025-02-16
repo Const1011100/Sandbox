44e160c4d776da5509d721883588c6211afb586a
Const1011100 <const1011100@gmail.com>
2025-02-16 20:14:29 +0200

    Codewars Task №643

--- js/codewars_641-660.js
+++ js/codewars_641-660.js
@@ -76,6 +76,55 @@ const disariumNumber_ = (n) =>
   } !!`;
 // ================================================================================================
 // ================================================================================================
+//*-Task №643 Balanced Number (Special Numbers Series #1 ) (5kyu)
+/*
+Збалансоване число — це число, в якому сума цифр ліворуч від середньої цифри (цифр) і сума цифр
+праворуч від середньої цифри (цифр) рівні.
+
+Якщо число має непарну кількість цифр, то середня цифра лише одна. (Наприклад, 92645 має одну середню
+цифру, 6.) В іншому випадку є дві середні цифри. (Наприклад, середніми цифрами 1301 є 3 і 0.)
+
+Середню(і) цифру(и) не слід враховувати під час визначення того, збалансоване число чи ні, напр.
+413023 є збалансованим числом, тому що ліва і права сума дорівнюють 5.
+
+завдання
+Дано число, знайдіть, чи воно збалансоване, і поверніть рядок 'Balanced' або 'Not Balanced'
+відповідно. Передане число завжди буде позитивним.
+
+7 ==> return "Balanced"
+295591 ==> return "Not Balanced"
+959 ==> return "Balanced"
+27102983 ==> return "Not Balanced"
+*/
+
+// Мій варіант
+function balancedNum(number) {
+  const numbers = `${number}`.split('').map(Number);
+  const len = numbers.length;
+
+  if (len < 2) return 'Balanced';
+
+  const mid = Math.floor(len / 2);
+  const leftSum = numbers
+    .slice(0, mid - (len % 2 === 0 ? 1 : 0))
+    .reduce((sum, num) => sum + num, 0);
+  const rightSum = numbers.slice(mid + 1).reduce((sum, num) => sum + num, 0);
+
+  return leftSum === rightSum ? 'Balanced' : 'Not Balanced';
+}
+
+// Цікаві варіанти з Codewars
+function balancedNum_(number) {
+  let str = `${number}`,
+    len = (str.length - (str.length % 2 ? -1 : -2)) / 2,
+    sum = (digits) => [...digits].reduce((a, b) => a + +b, 0);
+
+  return sum(str.slice(0, len)) === sum(str.slice(-len))
+    ? 'Balanced'
+    : 'Not Balanced';
+}
+// ================================================================================================
+// ================================================================================================
 //*-залишилось 1742шт
 // console.time('timer_1');
 // console.timeEnd('timer_1');
