1eb7b5e34a9a7f6c4178ba28f566fd7963ed9b64
Const1011100 <const1011100@gmail.com>
2025-06-01 15:39:03 +0300

    Codewars Task №728

--- js/codewars_721-740.js
+++ js/codewars_721-740.js
@@ -275,5 +275,57 @@ const findSpaceship_ = (map = '') => {
 };
 // ================================================================================================
 // ================================================================================================
+//*-Task №728 Credit card issuer checking (7kyu)
+/*
+Маючи номер кредитної картки, ми можемо визначити, хто є емітентом/постачальником, маючи кілька
+основних відомих даних.
+
+Заповніть функцію getIssuer(), яка використовуватиме значення, наведені нижче, для визначення
+емітента картки для заданого номера картки. Якщо номер не може бути збіганий, функція повинна
+повернути рядок Unknown.
+| Card Type  | Begins With          | Number Length |
+|------------|----------------------|---------------|
+| AMEX       | 34 or 37             | 15            |
+| Discover   | 6011                 | 16            |
+| Mastercard | 51, 52, 53, 54 or 55 | 16            |
+| VISA       | 4                    | 13 or 16      |
+getIssuer(4111111111111111) == "VISA"
+getIssuer(4111111111111) == "VISA"
+getIssuer(4012888888881881) == "VISA"
+getIssuer(378282246310005) == "AMEX"
+getIssuer(6011111111111117) == "Discover"
+getIssuer(5105105105105100) == "Mastercard"
+getIssuer(5105105105105106) == "Mastercard"
+getIssuer(9111111111111111) == "Unknown"
+*/
+
+// Мій варіант
+function getIssuer(number) {
+  const strNum = `${number}`;
+  if (strNum.length === 15 && /^3[47]/.test(strNum)) {
+    return 'AMEX';
+  }
+  if (strNum.length === 16 && /^6011/.test(strNum)) {
+    return 'Discover';
+  }
+  if (strNum.length === 16 && /^5[12345]/.test(strNum)) {
+    return 'Mastercard';
+  }
+  if ((strNum.length === 13 || strNum.length === 16) && strNum[0] === '4') {
+    return 'VISA';
+  }
+  return 'Unknown';
+}
+// Цікаві варіанти з Codewars
+function getIssuer_(n) {
+  var s = n.toString();
+  if (/^3[4|7]\d{13}$/.test(s)) return 'AMEX';
+  if (/^6011\d{12}$/.test(s)) return 'Discover';
+  if (/^5[1-5]\d{14}$/.test(s)) return 'Mastercard';
+  if (/^4(\d{12}|\d{15})$/.test(s)) return 'VISA';
+  return 'Unknown';
+}
+// ================================================================================================
+// ================================================================================================
 // console.time('timer_1');
 // console.timeEnd('timer_1');
