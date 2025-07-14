c62defd73896f4801f25a01d3334d5cb39ca614d
Const1011100 <const1011100@gmail.com>
2025-07-14 22:31:45 +0300

    Codewars Task №743

--- js/codewars_741-760.js
+++ js/codewars_741-760.js
@@ -39,5 +39,32 @@ function reverseBits(n) {
 
 // ================================================================================================
 // ================================================================================================
+//*-Task №743 shorter concat [reverse longer] (7kyu)
+/*
+Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+If a and b have the same length treat a as the longer producing b+reverse(a)+b
+ */
+
+// Мій варіант
+function shorterReverseLonger(a, b) {
+  if (a.length >= b.length) {
+    const c = a.split('').reverse().join('');
+    return b + c + b;
+  }
+  if (a.length < b.length) {
+    const c = b.split('').reverse().join('');
+    return a + c + a;
+  }
+}
+// Цікаві варіанти з Codewars
+function shorter_reverse_longer_(a, b) {
+  return a.length >= b.length
+    ? b + a.split('').reverse().join('') + b
+    : a + b.split('').reverse().join('') + a;
+}
+// ================================================================================================
+// ================================================================================================
 // console.time('timer_1');
 // console.timeEnd('timer_1');
